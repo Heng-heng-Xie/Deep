a5ccde28136d6e7925e1664650f258d2f7995753
Heng-heng-Xie <hhxie@utexas.edu>
2023-07-21 18:44:39 -0500

    !HW_finaland HW_5 no video!

--- homework5/homework/utils.py
+++ homework5/homework/utils.py
@@ -79,7 +79,7 @@ class PyTux:
         :return: Number of steps played
         """
 
-        import io
+
 
         if self.k is not None and self.k.config.track == track:
             self.k.restart()
@@ -104,7 +104,7 @@ class PyTux:
             import matplotlib.pyplot as plt
             fig, ax = plt.subplots(1, 1)
 
-        frames = []
+
 
         for t in range(max_frames):
 
@@ -148,22 +148,12 @@ class PyTux:
                     ax.add_artist(plt.Circle(WH2*(1+aim_point_image), 2, ec='g', fill=False, lw=1.5))
                 plt.pause(1e-3)
 
-                with io.BytesIO() as buff:
-                    fig.savefig(buff, format='raw')
-                    buff.seek(0)
-                    data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
-                w, h = fig.canvas.get_width_height()
-                im = data.reshape((int(h), int(w), -1))
 
-                frames.append(im)
 
             self.k.step(action)
             t += 1
-        if verbose:
-            import imageio
 
-            imageio.mimwrite("test.mp4", frames, fps=30, bitrate=1000000)
-            return t, kart.overall_distance / track.length
+
 
         return t, kart.overall_distance / track.length
 
